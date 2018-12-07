ae86e29dbab170b0e87311908f6beb0e08e548c4
jeannadolny <jeannadolny@hotmail.com>
2018-12-07 18:27:18 -0500

    novo

--- src/main/java/com/example/Quadrado.java
+++ src/main/java/com/example/Quadrado.java
@@ -6,7 +6,7 @@ public class Quadrado {
     
   }
 	public static boolean compara(double i) {
-		Double resultado = Math.sqrt(i);
+		double resultado = Math.sqrt(i);
 		if (resultado != null) {
 			return (resultado.intValue() == resultado.doubleValue());
 		} else {
