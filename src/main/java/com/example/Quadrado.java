cd6f47d2e20f05220a18b28c4ed0872a51f1158c
jeannadolny <jeannadolny@hotmail.com>
2018-12-07 17:48:19 -0500

    novo

--- src/main/java/com/example/Quadrado.java
+++ src/main/java/com/example/Quadrado.java
@@ -10,12 +10,4 @@ public class Quadrado {
 			return false;
 		}
 	}
-	public static void main(String args[]) {
-		double i = 100;
-		if (compara(i)) {
-			System.out.println("Quadrado Perfeito");
-		} else {
-			System.out.println("Não é quadrado perfeito");
-		}
-	}
 }
